a2f4bf169fe8706236732d3f4fff3f258acd6603
joey <joeydangg@gmail.com>
2021-11-04 15:22:59 -0700

    Make general recipe card layout

---
+++ components/RecipeCard.js
@@ -0,0 +1,30 @@
+const link = document.createElement('link');
+link.rel = 'stylesheet'; 
+link.type = 'text/css';
+link.href = 'styles/recipe-card.css'; 
+
+const recipeCardTemplate = document.createElement('template');
+recipeCardTemplate.innerHTML = `
+  <article class="recipe-card">
+    <img class="recipe-favorite" src="assets/favorite.svg" alt="favorite" />
+    <div class="recipe-image">
+    <div class="recipe-subdescription">
+      <p class="recipe-name">Lorem ipsum dolor sit amet </p>
+      <p class="recipe-calories"><spa class="recipe-calories-number">500</span> calories</p>
+    </div>
+  </article>
+`
+
+class RecipeCard extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.shadow.appendChild(link);
+    this.shadow.appendChild(recipeCardTemplate.content);
+  }
+}
+
+customElements.define('recipe-card-component', RecipeCard);
