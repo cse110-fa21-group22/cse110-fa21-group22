f9d66d42f0e2fbd5b277d263becd6a619ed23062
joey <joeydangg@gmail.com>
2021-11-05 13:39:12 -0700

    Fix recipe card styling and issues

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -7,9 +7,7 @@ const recipeCardTemplate = document.createElement('template');
 recipeCardTemplate.innerHTML = `
   <article class="recipe-card">
     <img class="recipe-favorite" src="assets/favorite.svg" alt="favorite" />
-    <div class="recipe-image">
-      <img><img/>
-    </div>
+      <img class="recipe-image"><img/>
     <div class="recipe-subdescription">
       <p class="recipe-name">Lorem ipsum dolor sit amet </p>
       <p class="recipe-calories"><span class="recipe-calories-number">500</span> calories</p>
@@ -21,7 +19,7 @@ class RecipeCard extends HTMLElement {
 
   set recipe(recipeObj) { 
     this.setAttribute('recipe-id', recipeObj['id']);
-    const recipeImg = this.shadow.querySelector('.recipe-image').querySelector('img');
+    const recipeImg = this.shadow.querySelector('.recipe-image');
     const recipeCal = this.shadow.querySelector('.recipe-calorie-number');
     const recipeName = this.shadow.querySelector('.recipe-name');
 
