142063c13e2f487f36aeaef1221331d690b9e766
Elliot Chung <ec2018@gmail.com>
2021-11-05 07:23:23 -0700

    Wrote fetching data for components and search page

--- scripts/search.js
+++ scripts/search.js
@@ -1,3 +1,66 @@
-/**
- * Handles all searching across all pages. 
- */
+const apiKey = 'bcccf988c95c45ef9b53310545b3989a';
+const tokenKey = '?apiKey=' + apiKey;
+
+window.addEventListener('DOMContentLoaded', init);
+
+
+
+function init () { 
+    const navbar = document.querySelector('navbar-component');
+    const searchKeyword = document.querySelector('.search-word'); 
+    const recipeSection = document.querySelector('.recipe-section'); 
+    
+    navbar.shadowRoot.addEventListener('submit', (event) => {
+        search(event.detail);
+    })
+
+    // This is just for testing
+    window.addEventListener('keyup', (event) => {
+        if (event.code == 'Escape') {
+            clear();
+        }
+    })
+
+    async function search(inputList) { 
+        const searchUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+        const tokenSearchInput = '&query=' + inputList['query'];
+        const tokenNumResults = '&number=' + inputList['number'];
+        const tokenOffset = '&offset=' + inputList['offset'];
+        const tokenNutritionBool = '&addRecipeNutrition=' + inputList['recipe-nutrition'];
+        const fetchEndpoint = searchUrl + tokenKey + tokenSearchInput + tokenNumResults + tokenOffset ;//+ tokenNutritionBool;
+
+        console.log(fetchEndpoint);
+        
+        let json;
+        try {
+            const data = await fetch(fetchEndpoint);
+            json = await data.json();
+        } catch (error) { 
+            console.error(error); 
+        }
+        const results  = json.results;
+    
+        searchKeyword.innerHTML = '\"' + inputList['query'] + '\"';
+        
+        console.log(results);
+
+        for (const recipe in results) { 
+            const recipeCard = document.createElement('recipe-card-component')
+            recipeCard.recipe = results[recipe];
+            recipeSection.appendChild(recipeCard);
+        }
+    
+    }
+
+    function clear() { 
+        while (recipeSection.firstChild) {
+            recipeSection.removeChild(recipeSection.firstChild);
+        }
+        
+    }
+}
+
+
+
+
+
