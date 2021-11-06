d02030b434aee584ec3ccca74e49001ba3948866
HairforceNiko <fayuan@ucsd.edu>
2021-11-05 18:14:51 -0700

    Update Navbar.js, navbar.css, home.html, navbar.html, search.html, favorite.css
    1. Rename the home_page.html to home.html
    2. Modify favorite.css. Change the height from 80px to 60px
    3. Modify navbar.css. Need to be reviewed.
    4. Modify Navbar.js to make it work
    5. Modify search.html to make it work

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,30 +1,30 @@
 const link = document.createElement('link');
 link.rel = 'stylesheet'; 
 link.type = 'text/css';
-link.href = 'styles/navbar.css'; 
+link.href = '../styles/navbar.css'; 
 
 const navbarTemplate = document.createElement('template');
 navbarTemplate.innerHTML = `
   <nav class="navbar">
-    <a href="/">
+    <a href="../">
       <div class="logo">Logo</div>
     </a>
     <div class="navbar-routes">
-      <img src="assets/search.svg" alt="search" />
+      <img src="../assets/search.svg" alt="search" />
       <div class='nav-search'>
         <input 
           type='text'
           placeholder="Search"
         />
       </div>
-      <a href="/favorite.html">
+      <a href="../webpages/favorite.html">
         <p class="nav-favorite">Favorite</p>
       </a>
-      <a href="/search.html">
-        <img class="nav-search-mobile" src="assets/search.svg" alt="search" />
+      <a href="../webpages/search.html">
+        <img class="nav-search-mobile" src="../assets/search.svg" alt="search" />
       </a>
-      <a href="/favorite.html">
-        <img class="nav-favorite-mobile" src="assets/favorite.svg" alt="favorite" />
+      <a href="../webpages/favorite.html">
+        <img class="nav-favorite-mobile" src="../assets/favorite.svg" alt="favorite" />
       </a>
     </div>
   </nav>
