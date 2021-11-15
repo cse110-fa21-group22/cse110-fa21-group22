d38354942568316f3f29ad3731965c0c1811c928
joey <joeydangg@gmail.com>
2021-11-14 17:33:05 -0800

    Update white space

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,9 @@
-const link = document.createElement('link');
-link.rel = 'stylesheet'; 
-link.type = 'text/css';
-link.href = '../styles/navbar.css'; 
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "../styles/navbar.css";
 
-const navbarTemplate = document.createElement('template');
+const navbarTemplate = document.createElement("template");
 navbarTemplate.innerHTML = `
 	<nav class="navbar">
 		<a href="/webpages/home.html">
@@ -23,59 +23,64 @@ navbarTemplate.innerHTML = `
 `;
 
 class Navbar extends HTMLElement {
-	/**
-	 * Contructor for the Navbar class
-	 */
-	constructor() {
-		super();
-		this.shadow = this.attachShadow({ mode: 'open' });
-	}
+  /**
+   * Contructor for the Navbar class
+   */
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
 
-	/**
-	 * Generates a query string to pass the search to the search page
-	 * @param {string} searchTerm the user's search phrase
-	 * @return {string} the query string
-	 */
-	generateQueryString(searchTerm) {
-		let queryString = '?search=';
-		for (let i = 0; i < searchTerm.length; i++) {
-			let currChar = searchTerm.charAt(i);
-			let currCharCode = searchTerm.charCodeAt(i);
-			console.log(currChar);
-			// Allow all letters and numbers to enter the query
-			if ((currCharCode >= 65 && currCharCode <= 90) ||
-				(currCharCode >= 97 && currCharCode <= 122) ||
-				(currCharCode >= 48 && currCharCode <= 57)) {
-				queryString += currChar;
-				// Spaces become + signs
-			} else if (currCharCode == '32') {
-				queryString += '+';
-				// All other characters become - signs
-			} else {
-				queryString += '-';
-			}
-		}
-		return queryString;
-	}
+  /**
+   * Generates a query string to pass the search to the search page
+   * @param {string} searchTerm the user's search phrase
+   * @return {string} the query string
+   */
+  generateQueryString(searchTerm) {
+    let queryString = "?search=";
+    for (let i = 0; i < searchTerm.length; i++) {
+      let currChar = searchTerm.charAt(i);
+      let currCharCode = searchTerm.charCodeAt(i);
+      console.log(currChar);
+      // Allow all letters and numbers to enter the query
+      if (
+        (currCharCode >= 65 && currCharCode <= 90) ||
+        (currCharCode >= 97 && currCharCode <= 122) ||
+        (currCharCode >= 48 && currCharCode <= 57)
+      ) {
+        queryString += currChar;
+        // Spaces become + signs
+      } else if (currCharCode == "32") {
+        queryString += "+";
+        // All other characters become - signs
+      } else {
+        queryString += "-";
+      }
+    }
+    return queryString;
+  }
 
-	connectedCallback() {
-		this.shadow.appendChild(link);
-		this.shadow.appendChild(navbarTemplate.content);
+  connectedCallback() {
+    this.shadow.appendChild(link);
+    this.shadow.appendChild(navbarTemplate.content);
 
-		//Desktop Functionality
-		let searchTerm = '';
-		// let inputList = [];
+    //Desktop Functionality
+    let searchTerm = "";
+    // let inputList = [];
 
-		const searchInput = this.shadow.querySelector('.nav-search').querySelector('input');
+    const searchInput = this.shadow
+      .querySelector(".nav-search")
+      .querySelector("input");
 
-		searchInput.addEventListener('keyup', (event) => {
-			searchTerm = event.target.value;
+    searchInput.addEventListener("keyup", (event) => {
+      searchTerm = event.target.value;
 
-			if (event.code == 'Enter') {
-				window.location.href = 'search.html' + this.generateQueryString(searchTerm);
-			}
-		});
-	}
+      if (event.code == "Enter") {
+        window.location.href =
+          "search.html" + this.generateQueryString(searchTerm);
+      }
+    });
+  }
 }
 
-customElements.define('navbar-component', Navbar);
+customElements.define("navbar-component", Navbar);
--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,9 +1,9 @@
-const link = document.createElement('link');
-link.rel = 'stylesheet'; 
-link.type = 'text/css';
-link.href = '../styles/recipeCard.css'; 
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "../styles/recipeCard.css";
 
-const recipeCardTemplate = document.createElement('template');
+const recipeCardTemplate = document.createElement("template");
 recipeCardTemplate.innerHTML = `
 	<article class="recipe-card">
     <img class="recipe-favorite" src="../assets/favorite.svg" alt="favorite" />
@@ -17,19 +17,19 @@ recipeCardTemplate.innerHTML = `
 
 class RecipeCard extends HTMLElement {
   set recipe(recipeObj) {
-    this.setAttribute('recipe-id', recipeObj['id']);
-    const recipeImg = this.shadow.querySelector('.recipe-image');
+    this.setAttribute("recipe-id", recipeObj["id"]);
+    const recipeImg = this.shadow.querySelector(".recipe-image");
     // const recipeCal = this.shadow.querySelector('.recipe-calorie-number');
-    const recipeName = this.shadow.querySelector('.recipe-name');
+    const recipeName = this.shadow.querySelector(".recipe-name");
 
-    recipeImg.src = recipeObj['image'];
-    recipeName.innerHTML = recipeObj['title'];
+    recipeImg.src = recipeObj["image"];
+    recipeName.innerHTML = recipeObj["title"];
     // recipeCal.innerHTML = recipeObj['calories'];
   }
 
   constructor() {
     super();
-    this.shadow = this.attachShadow({ mode: 'open' });
+    this.shadow = this.attachShadow({ mode: "open" });
     this.shadow.appendChild(recipeCardTemplate.content.cloneNode(true));
     this.shadow.appendChild(link.cloneNode(true));
   }
@@ -38,38 +38,38 @@ class RecipeCard extends HTMLElement {
     let isFavorite = false;
 
     // If the recipe card is clicked, move to the recipe page
-    this.addEventListener('click', () => {
-      window.location.href = 'recipe.html?id=' + this.getAttribute('recipe-id');
+    this.addEventListener("click", () => {
+      window.location.href = "recipe.html?id=" + this.getAttribute("recipe-id");
     });
 
     // If the favorite icon is clicked, favorite the item
-    let favoriteIcon = this.shadow.querySelector('.recipe-favorite');
-    favoriteIcon.addEventListener('click', (event) => {
+    let favoriteIcon = this.shadow.querySelector(".recipe-favorite");
+    favoriteIcon.addEventListener("click", (event) => {
       // Stop propagation to the parent so you don't go to the recipe page
       event.stopPropagation();
-      console.log('clicked favorite icon');
+      console.log("clicked favorite icon");
       if (!isFavorite) {
         isFavorite = true;
-        favoriteIcon.src = '../assets/favorite-selected.svg';
+        favoriteIcon.src = "../assets/favorite-selected.svg";
         // add item to favorites list here
       } else {
         isFavorite = false;
-        favoriteIcon.src = '../assets/favorite.svg';
+        favoriteIcon.src = "../assets/favorite.svg";
         // remove item from favorites list here
       }
     });
 
     // Mouse hover for favorites icon
-    favoriteIcon.addEventListener('mouseover', () => {
-      favoriteIcon.src = '../assets/favorite-selected.svg';
+    favoriteIcon.addEventListener("mouseover", () => {
+      favoriteIcon.src = "../assets/favorite-selected.svg";
     });
 
-    favoriteIcon.addEventListener('mouseout', () => {
+    favoriteIcon.addEventListener("mouseout", () => {
       if (!isFavorite) {
-        favoriteIcon.src = '../assets/favorite.svg';
+        favoriteIcon.src = "../assets/favorite.svg";
       }
     });
   }
 }
 
-customElements.define('recipe-card-component', RecipeCard);
+customElements.define("recipe-card-component", RecipeCard);
