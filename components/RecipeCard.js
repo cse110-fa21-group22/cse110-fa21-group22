65cb69cc6efaa90ce05960de5586e8034bbe60b1
joey <joeydangg@gmail.com>
2021-11-14 17:44:54 -0800

    Update white spaces and quotes

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,9 +1,9 @@
-const link = document.createElement("link");
-link.rel = "stylesheet";
-link.type = "text/css";
-link.href = "../styles/recipeCard.css";
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = '../styles/recipeCard.css';
 
-const recipeCardTemplate = document.createElement("template");
+const recipeCardTemplate = document.createElement('template');
 recipeCardTemplate.innerHTML = `
 	<article class="recipe-card">
     <img class="recipe-favorite" src="../assets/favorite.svg" alt="favorite" />
@@ -16,60 +16,60 @@ recipeCardTemplate.innerHTML = `
 `;
 
 class RecipeCard extends HTMLElement {
-  set recipe(recipeObj) {
-    this.setAttribute("recipe-id", recipeObj["id"]);
-    const recipeImg = this.shadow.querySelector(".recipe-image");
-    // const recipeCal = this.shadow.querySelector('.recipe-calorie-number');
-    const recipeName = this.shadow.querySelector(".recipe-name");
+	set recipe(recipeObj) {
+		this.setAttribute('recipe-id', recipeObj['id']);
+		const recipeImg = this.shadow.querySelector('.recipe-image');
+		// const recipeCal = this.shadow.querySelector('.recipe-calorie-number');
+		const recipeName = this.shadow.querySelector('.recipe-name');
 
-    recipeImg.src = recipeObj["image"];
-    recipeName.innerHTML = recipeObj["title"];
-    // recipeCal.innerHTML = recipeObj['calories'];
-  }
+		recipeImg.src = recipeObj['image'];
+		recipeName.innerHTML = recipeObj['title'];
+		// recipeCal.innerHTML = recipeObj['calories'];
+	}
 
-  constructor() {
-    super();
-    this.shadow = this.attachShadow({ mode: "open" });
-    this.shadow.appendChild(recipeCardTemplate.content.cloneNode(true));
-    this.shadow.appendChild(link.cloneNode(true));
-  }
+	constructor() {
+		super();
+		this.shadow = this.attachShadow({ mode: 'open' });
+		this.shadow.appendChild(recipeCardTemplate.content.cloneNode(true));
+		this.shadow.appendChild(link.cloneNode(true));
+	}
 
-  connectedCallback() {
-    let isFavorite = false;
+	connectedCallback() {
+		let isFavorite = false;
 
-    // If the recipe card is clicked, move to the recipe page
-    this.addEventListener("click", () => {
-      window.location.href = "recipe.html?id=" + this.getAttribute("recipe-id");
-    });
+		// If the recipe card is clicked, move to the recipe page
+		this.addEventListener('click', () => {
+			window.location.href = 'recipe.html?id=' + this.getAttribute('recipe-id');
+		});
 
-    // If the favorite icon is clicked, favorite the item
-    let favoriteIcon = this.shadow.querySelector(".recipe-favorite");
-    favoriteIcon.addEventListener("click", (event) => {
-      // Stop propagation to the parent so you don't go to the recipe page
-      event.stopPropagation();
-      console.log("clicked favorite icon");
-      if (!isFavorite) {
-        isFavorite = true;
-        favoriteIcon.src = "../assets/favorite-selected.svg";
-        // add item to favorites list here
-      } else {
-        isFavorite = false;
-        favoriteIcon.src = "../assets/favorite.svg";
-        // remove item from favorites list here
-      }
-    });
+		// If the favorite icon is clicked, favorite the item
+		let favoriteIcon = this.shadow.querySelector('.recipe-favorite');
+		favoriteIcon.addEventListener('click', (event) => {
+			// Stop propagation to the parent so you don't go to the recipe page
+			event.stopPropagation();
+			console.log('clicked favorite icon');
+			if (!isFavorite) {
+				isFavorite = true;
+				favoriteIcon.src = '../assets/favorite-selected.svg';
+				// add item to favorites list here
+			} else {
+				isFavorite = false;
+				favoriteIcon.src = '../assets/favorite.svg';
+				// remove item from favorites list here
+			}
+		});
 
-    // Mouse hover for favorites icon
-    favoriteIcon.addEventListener("mouseover", () => {
-      favoriteIcon.src = "../assets/favorite-selected.svg";
-    });
+		// Mouse hover for favorites icon
+		favoriteIcon.addEventListener('mouseover', () => {
+			favoriteIcon.src = '../assets/favorite-selected.svg';
+		});
 
-    favoriteIcon.addEventListener("mouseout", () => {
-      if (!isFavorite) {
-        favoriteIcon.src = "../assets/favorite.svg";
-      }
-    });
-  }
+		favoriteIcon.addEventListener('mouseout', () => {
+			if (!isFavorite) {
+				favoriteIcon.src = '../assets/favorite.svg';
+			}
+		});
+	}
 }
 
-customElements.define("recipe-card-component", RecipeCard);
+customElements.define('recipe-card-component', RecipeCard);
