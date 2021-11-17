4de4f03a067cbcc0a9ecfd647251a505599a16fb
Danlandy <74330177+Danlandy@users.noreply.github.com>
2021-11-16 17:46:03 -0800

    change design

--- components/UserLocalStorage.js
+++ components/UserLocalStorage.js
@@ -1,58 +1,86 @@
-export const storage = {};
 
-storage['favorite-master'] = {};
+// must have 'favorites-master'
+const store = window.localStorage; 
+var arr_temp = []; 
+store.setItem('favorites-master', JSON.stringify(arr_temp)); 
 
-storage.addToList = function (ListName, recipeID) {
-    localStoragetoStorage();
-    storage[ListName].push(recipeID);
-    StoragetoLocalStorage();
+
+export function addRecipe(recipeID) {
+    const storage = window.localStorage;
+    var list = storage.getItem('favorites-master'); 
+
+    var array = JSON.parse(list);
+    array.push(recipeID); 
+
+    storage.setItem('favorites-master', JSON.stringify(array)); 
 }
 
-storage.removeFromList = function (ListName, recipeID) {
-    localStoragetoStorage();
-    if (storage[ListName] != undefined) {
-        delete storage[ListName][recipeID];
-    }
-    StoragetoLocalStorage();
-}
 
-storage.createList = function (ListName) {
-    localStoragetoStorage();
-    if (storage[ListName] == undefined) {
-        storage[ListName] = {};
-    }
-    StoragetoLocalStorage();
-}
 
-storage.removeList = function (ListName) {
-    localStoragetoStorage();
-    if (storage[ListName] != undefined) {
-        delete storage[ListName];
-    }
-    StoragetoLocalStorage();
-}
 
-storage.addRecipe = function (recipeOBJ) {
-    // console.log(recipeOBJ); 
-    localStoragetoStorage();
-    storage['favorite-master'][recipeOBJ.id] = recipeOBJ;
-    StoragetoLocalStorage();
-}
 
-storage.removeRecipe = function (recipeID) {
-    localStoragetoStorage();
-    delete storage['favorite-master'][recipeID];
-    StoragetoLocalStorage();
-}
 
-function StoragetoLocalStorage() {
-    for (list in storage) {
-        localStorage.setItem(list, JSON.stringify(storage[list]));
-    }
-}
 
-function localStoragetoStorage() {
-    for (let i = 0; i < localStorage.length; i++) {
-        storage[localStorage.key(i)] = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    }
-}
+// export const storage = {};
+// const functions = ["addToList", "removeFromList", "createList", "removeList", "addRecipe", "removeRecipe"]; 
+// const dataHolder = {}; 
+
+// // holds the data, an array of ids
+// dataHolder['favorite-master'] = [];
+
+// storage.addToList = function (ListName, recipeID) {
+//     localStoragetoStorage();
+//     dataHolder[ListName].push(recipeID);
+//     StoragetoLocalStorage();
+// }
+
+// storage.removeFromList = function (ListName, recipeID) {
+//     localStoragetoStorage();
+//     if (dataHolder[ListName] != undefined) {
+//         delete dataHolder[ListName][recipeID];
+//     }
+//     StoragetoLocalStorage();
+// }
+
+// storage.createList = function (ListName) {
+//     localStoragetoStorage();
+//     if (dataHolder[ListName] == undefined) {
+//         dataHolder[ListName] = {};
+//     }
+//     StoragetoLocalStorage();
+// }
+
+// storage.removeList = function (ListName) {
+//     localStoragetoStorage();
+//     if (dataHolder[ListName] != undefined) {
+//         delete dataHolder[ListName];
+//     }
+//     StoragetoLocalStorage();
+// }
+
+// storage.addRecipe = function (recipeID) {
+//     // console.log(recipeOBJ); 
+//     localStoragetoStorage();
+//     dataHolder['favorite-master'][recipeOBJ.id] = JSON.parse(recipeOBJ);
+//     StoragetoLocalStorage();
+// }
+
+// storage.removeRecipe = function (recipeID) {
+//     localStoragetoStorage();
+//     delete dataHolder['favorite-master'][recipeID];
+//     StoragetoLocalStorage();
+// }
+
+// function StoragetoLocalStorage() {
+//     for (let list in dataHolder) {
+//         localStorage.setItem(list, JSON.stringify(JSON.parse(dataHolder[list])));
+//     }
+// }
+
+// function localStoragetoStorage() {
+//     for (let i = 0; i < localStorage.length; i++) {
+//         if(functions.includes(localStorage.key(i)) == false){
+//             dataHolder[localStorage.key(i)] = JSON.parse(localStorage.getItem(localStorage.key(i)));
+//         }
+//     }
+// }
--- scripts/homepage.js
+++ scripts/homepage.js
@@ -2,15 +2,17 @@
  * API key for connecting with Spoonatular API
  */
 // import {apiKey} from './apikey.js';
-const apiKey = '8581385ca4af4148b1a78b5ef23e5b8c'; 
-import {storage} from "/components/UserLocalStorage.js"; 
+const apiKey = '8581385ca4af4148b1a78b5ef23e5b8c';
+import { addRecipe } from "../components/UserLocalStorage.js";
 
 const tokenKey = '?apiKey=' + apiKey;
+const storage = window.localStorage; 
+
 
 const stepping_size = 2; // Stepping-size number of recipes to append to end after user scrolls to bottom
 const recipeData = {}; // Data in each stepping size?
 const recipeSection = document.querySelector('.home-page-popular-recipe-list'); // Where to place recipe cards
-const userFavoriteSection = document.querySelector('.home-page-favorite-section'); 
+const userFavoriteSection = document.querySelector('.home-page-favorite-section');
 
 window.addEventListener('DOMContentLoaded', init);
 async function init() {
@@ -43,13 +45,12 @@ async function init() {
 	});
 
 	// populating local storage 
-	// console.log(storage); 
-	// var try_delete; 
-	// for(let a in recipeData) {
-	// 	try_delete = a; 
-	// 	break; 
-	// }
-	// storage.removeRecipe(try_delete); 
+	var list = storage.getItem('favorites-master');
+	console.log(list);
+
+	// return null if not found 
+	// let a = storage.getItem("dsf");
+	// console.log(a);
 
 	showFavoriteSection(); 
 }
@@ -73,13 +74,14 @@ async function fetch_random_recipes() {
 				// console.log(data.recipes[0].id);
 				for (let i = 0; i < parseInt(stepping_size); i++) {
 					recipeData[data.recipes[i].id] = data.recipes[i];
-					// testign 
-					storage.addRecipe(data.recipes[i]); 
+					// testing
+					addRecipe(data.recipes[i].id);
 				}
 				resolve();
 			})
 			.catch((err) => {
 				console.log('Error loading the recipe');
+				console.log(err);
 				reject(err);
 			});
 	});
@@ -119,10 +121,35 @@ function clearObject() {
 	for (var member in recipeData) delete recipeData[member];
 }
 
-function showFavoriteSection(){
-	for (const recipeid in storage['favorite-master']) {
+async function showFavoriteSection() {
+    var list = storage.getItem('favorites-master'); 
+    var array = JSON.parse(list);
+	console.log(array); 
+
+	for (let i = 0; i < array.length; i ++) {
 		const recipeCard = document.createElement('recipe-card-component');
-		recipeCard.recipe = storage['favorite-master'][recipeid];
+		recipeCard.recipe = await getRecipebyID(array[i]); 
+
 		userFavoriteSection.appendChild(recipeCard);
 	}
+}
+
+async function getRecipebyID(id) {
+	const fetchEndPoint =
+		'https://api.spoonacular.com/recipes/' +
+		id + '/' + "information" + tokenKey + 
+		"&includeNutrition=false"; 
+		 
+
+	console.log("fetch_endpoint", fetchEndPoint); 
+
+	const fetchResults = await fetch(fetchEndPoint)
+		.then((response) => response.json())
+		.catch((error) => {
+			console.error('Fetch in homepage failed');
+			console.error(error);
+		});
+
+	console.log("result is: ", fetchResults);
+	return fetchResults;
 }
