c904cc4b551845a4bcee221b286fce71581cff32
mtherber <mtherber@ucsd.edu>
2021-11-16 20:40:49 -0800

    Added styling to edit mode, got basic select functionality working

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -3,12 +3,6 @@
  */
 
 
-let editMode = false;
-let editButton = document.getElementById('edit');
-let cancelButton = document.getElementById('cancel');
-let moveButton = document.getElementById('move');
-
-
 /**
  * A TEST FUNCTION FOR POPULATING RECIPES ON FAVORITES PAGE
  * @return {array} of the recipe cards
@@ -16,24 +10,35 @@ let moveButton = document.getElementById('move');
 function populateRecipeSection() {
 	// Add the recipes to the page
     let result = [];
+    let recipeSection = document.querySelector('.recipe-section');
     for (let i = 0; i < 10; i++) {
         const recipeCard = document.createElement('recipe-card-component');
         recipeSection.appendChild(recipeCard);
         result.push(recipeCard);
+        recipeCard.recipeCardSelect = false;
     }
+    return result;
 }
+let recipeCards = populateRecipeSection();
+
+
+let editMode = false;
+let recipeCardSelect = false;
+let editButton = document.getElementById('edit');
+let cancelButton = document.getElementById('cancel');
+let moveButton = document.getElementById('move');
 
 editButton.addEventListener('click', function() {
-    if (editMode) {
-        // edit mode was already true
-    } else {
-        console.log('edit mode activated');
+    if (!editMode) {
         editMode = true;
-        document.body.style.backgroundColor = '#DDDDDD';
+        document.body.style.backgroundColor = '#EEEEEE';
         editButton.style.display = 'none';
         cancelButton.style.display = 'inline-block';
         moveButton.style.display = 'inline-block';
-
+        for (let i = 0; i < recipeCards.length; i++) {
+            recipeCards[i].enterSelectMode();
+        }
+        recipeCardSelect = true;
     }
 });
 
@@ -45,7 +50,9 @@ cancelButton.addEventListener('click', function() {
         editButton.style.display = 'inline-block';
         cancelButton.style.display = 'none';
         moveButton.style.display = 'none';
-    } else {
-        // edit mode has already been cancelled
+        for (let i = 0; i < recipeCards.length; i++) {
+            recipeCards[i].exitSelectMode();
+        }
+        recipeCardSelect = false;
     }
 });
