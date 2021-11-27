3b8f2ca16d4b1c94eb4fc331d1228bc0f714742f
David Em-Yinn <demyinn@ucsd.edu>
2021-11-26 18:04:36 -0800

    fixed next page test

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -1,23 +1,25 @@
-describe('Basic user flow for Search Page', () => {
-    jest.setTimeout(10000);
+jest.setTimeout(10000);
 
+describe('Basic user flow for Search Page', () => {
     // Launch the site
     beforeAll(async () => {
-        await page.goto('https://619eba98b1f5aa97eda3fee5--icookfood.netlify.app/webpages/search.html');
+        await page.goto('https://icookfood.netlify.app/webpages/search.html');
         await page.waitForTimeout(800);
     });
 
+    let firstSet = [];
     // Check the search page without a search
     it('Check for 10 default recipes on empty search', async () => {
         console.log('Check for 10 default recipe');
         await page.waitForTimeout('500');
 
-        const recipeCards = await page.$$eval('section', (sections) => {
-          return sections[0].querySelectorAll('recipe-card-component').length;
+        const numRecipeCards = await page.$$eval('recipe-card-component', (recipeCards) => {
+          firstSet = recipeCards;
+          return recipeCards.length;//sections[0].querySelectorAll('recipe-card-component').length;
         });
         
-        expect(recipeCards).toBe(10);
-      });
+        expect(numRecipeCards).toBe(10);
+      }, 10000);
 
       /*
     
@@ -47,23 +49,28 @@ describe('Basic user flow for Search Page', () => {
         expect(recipeCards).toBe(10);
     });
 
-    
-
+    */
+   
+    let secondSet = [];
     // User clicks the next results button, make sure there recipe cards are populated
-    it('Check next results button', () => {
+    it('Check next results button', async () => {
         console.log('Check next button');
-        await page.waitForTimeout('500');
+        // await page.waitForTimeout('500');
 
         let nextButton = await page.$('.next-button');
+       
         await nextButton.click();
         await page.waitForTimeout('500');
 
-        const recipeCards = await page.$$eval('.recipe-section', (sections) => {
-            return sections[0].querySelectorAll('recipe-card-component').length;
-          });
+        const recipeCards = await page.$$('recipe-card-component');
+        secondSet = recipeCards;
+        // const recipeCards = await page.$$eval('.recipe-section', (sections) => {
+        //     for (let i = 0; i < sections[0].querySelectorAll[0])
+        //     return sections[0].querySelectorAll('recipe-card-component').length;
+        //   });
         
-        expect(recipeCards).toBe(10);
+        expect(recipeCards.length).toBe(10);
     });
 
-    */
+    
 });
