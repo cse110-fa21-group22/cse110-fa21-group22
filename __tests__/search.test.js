a511c22d5de173ac66eaca2bbe1ac46a420512b4
David Em-Yinn <demyinn@ucsd.edu>
2021-11-26 17:17:35 -0800

    adding next button test and need to fix other tests

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -19,26 +19,51 @@ describe('Basic user flow for Search Page', () => {
         expect(recipeCards).toBe(10);
       });
 
+      /*
+    
     // Search for basic recipe
+    // Needs some work
     it('Inital search', async () => {
-        const navBarComp = await page.$('navbar-component');
-        const root = await navBarComp.getProperty('shadowRoot');
-        //const navBarSearchInput = await root.$('.nav-search-input');
-        //await root.type('.nav-search-input', 'chicken');
+        // const navBarComp = await page.$('navbar-component');
+        // const root = await navBarComp.getProperty('shadowRoot');
+        // //const navBarSearchInput = await root.$('.nav-search-input');
+        // //await root.type('.nav-search-input', 'chicken');
         
-        const navBar = await root.$('.navbar');//.$('.nav-search').$('input').value = 'chicken';
-        const navSearch = await navBar.$('.nav-search');
-        let input = await navSearch.$('input');
-        input.value = 'chicken';
+        // const navBar = await root.$('.navbar');//.$('.nav-search').$('input').value = 'chicken';
+        // const navSearch = await navBar.$('.nav-search');
+        // let input = await navSearch.$('input');
+        // input.value = 'chicken';
 
-        await page.reload();
+        await page.goto('https://619eba98b1f5aa97eda3fee5--icookfood.netlify.app/webpages/search.html?search=chicken');
+        //await page.reload();
         await page.waitForTimeout('500');
 
         // Checks search result to be 10 recipes 
-        const recipeCards = await page.$$eval('section', (sections) => {
+        // Will need to modify this to check for chicken recipes 
+        const recipeCards = await page.$$eval('.recipe-section', (sections) => {
             return sections[0].querySelectorAll('recipe-card-component').length;
           });
           
-          expect(recipeCards).toBe(10);
+        expect(recipeCards).toBe(10);
     });
+
+    
+
+    // User clicks the next results button, make sure there recipe cards are populated
+    it('Check next results button', () => {
+        console.log('Check next button');
+        await page.waitForTimeout('500');
+
+        let nextButton = await page.$('.next-button');
+        await nextButton.click();
+        await page.waitForTimeout('500');
+
+        const recipeCards = await page.$$eval('.recipe-section', (sections) => {
+            return sections[0].querySelectorAll('recipe-card-component').length;
+          });
+        
+        expect(recipeCards).toBe(10);
+    });
+
+    */
 });
