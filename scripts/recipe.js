061b6334a060572e1107e7bc61c0358f90ece7ae
samanthaylin3 <73860504+samanthaylin3@users.noreply.github.com>
2021-12-03 12:53:53 -0800

    unheart confirm on recipe card

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -213,7 +213,7 @@ async function init() {
       showDropdown(recipe, isMobile);
     } else {
       let toRemove = false;
-      toRemove = confirm(`Unhearting a recipe removes from all favorite lists`);
+      toRemove = confirm(`Unhearting a recipe removes from all favorite lists. To delete only from this list, try edit mode on favorite page.`);
       
       if (toRemove) {
         isFavorite = false;
