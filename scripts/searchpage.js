a248e5635fac8a58c5d1a9d8e338427030aa05f6
HairforceNiko <fayuan@ucsd.edu>
2021-12-03 11:14:45 -0800

    Update searchpage.js

--- scripts/searchpage.js
+++ scripts/searchpage.js
@@ -28,6 +28,8 @@ function init() {
   // intiallize the sidebar
   const navbarComponent = document.querySelector('navbar-component');
   const sidebarContent = navbarComponent.shadow.querySelector('.sidebar-content');
+  const sidebarHeight = document.body.scrollHeight;
+  sidebarContent.style.height = sidebarHeight;
   if (!isMobile && sidebarContent.style.display !== 'none') {
     sidebarContent.style.display = 'flex';
     sidebarContent.style.flexDirection = 'column';
