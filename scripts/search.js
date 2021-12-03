5526a778901f179444df459cf8506b57c3797244
HairforceNiko <fayuan@ucsd.edu>
2021-12-03 10:40:06 -0800

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -8,15 +8,15 @@ const tokenKey = `?apiKey=${apiKey}`;
 /**
  * Performs a simple text search through spoonacular
  * @param {object} inputList an object as defined below
- * inputList['query'] is the search phrase as a string
- * inputList['number'] is the number of results as a number
- * inputList['offset'] is the number of results to skip as a number
- * inputList['recipe-nutrition'] is a boolean specifying whether to show nutrition
  * @returns {object} json containing the results of the spoonacular query
  */
 export default async function search(inputList) {
-  // The order of the token need to be fixed is something wrong happened.
+  /**
+   * @inner
+   * @param {string} searchUrl the url for search
+   */
   const searchUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+  // The order of the token need to be fixed is something wrong happened.
   const tokenSearchInput = `&query=${inputList.query}`;
   const tokenNumResults = `&number=${inputList.number}`;
   const tokenOffset = `&offset=${inputList.offset}`;
