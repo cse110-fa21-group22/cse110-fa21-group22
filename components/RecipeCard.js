d28a5ecdb8c473fa79320df6b607dac99f859874
samanthaylin3 <73860504+samanthaylin3@users.noreply.github.com>
2021-12-03 16:09:51 -0800

    disable line

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -220,10 +220,8 @@ class RecipeCard extends HTMLElement {
         console.log('Prompting user to add to favorites lists');
       } else {
         let toRemove = false;
-        customConfirm("Are you sure?");
-        // customConfirm(`Unhearting a recipe removes from all favorite lists. To delete only from this list, try edit mode on favorite page. Are you sure you want to continue?`);
-        let confirm = myCustomLib.customConfirm;
-        toRemove = confirm();
+        // eslint-disable-next-line
+        toRemove = window.confirm(`Unhearting a recipe removes from all favorite lists. To delete only from this list, try edit mode on favorite page. Are you sure you want to continue?`);
         // continues to remove the recipe from list if user confirms they want to remove
         if (toRemove) {
           this.isFavorite = false;
