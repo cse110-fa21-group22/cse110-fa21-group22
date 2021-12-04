5e0a81f64c5f048aef6606875c28eb0f14f9966e
HairforceNiko <fayuan@ucsd.edu>
2021-12-03 16:13:49 -0800

    Update searchpage.js

--- scripts/searchpage.js
+++ scripts/searchpage.js
@@ -110,6 +110,8 @@ function init() {
   // Automatically parse the query string and run a search on page load
   const searchTerm = parseQueryString();
   console.log(searchTerm);
+  const navbarInputbox = document.querySelector('.navbar').shadow.querySelector('.nav-search-input');
+  navbarInputbox.innerHTML = `${searchTerm}`;
 
   // Send a query to spoonacular
   let inputList = [];
