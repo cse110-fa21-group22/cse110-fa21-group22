e4e13b0b7978a4b38d35526d6ea6105242470670
HairforceNiko <fayuan@ucsd.edu>
2021-12-05 01:34:37 -0800

    Update end-to-end.test.js

--- source/__tests__/end-to-end.test.js
+++ source/__tests__/end-to-end.test.js
@@ -22,7 +22,7 @@ let favSelected = '';
 
 if (prNum > 0) {
   homeLink = `${PREVIEW_TAG}${prNum}${END_TAG}webpages/home.html`;
-  searchLink = `${PREVIEW_TAG}${prNum}${END_TAG}webpages/search.html?search=`;
+  searchLink = `${PREVIEW_TAG}${prNum}${END_TAG}webpages/search.html`;
   favLink = `${PREVIEW_TAG}${prNum}${END_TAG}webpages/favorite.html`;
   favSelected = `${PREVIEW_TAG}${prNum}${END_TAG}assets/favorite-selected.svg`;
 } else {
