b652fe90f284f2c86d3b13a240612f3fbc67ad11
Emily Ekaireb <eekaireb@ucsd.edu>
2021-12-04 22:11:33 -0800

    fix lint

--- scripts/search.js
+++ scripts/search.js
@@ -1,8 +1,9 @@
 /**
  * Handles all queries related to spoonacular api.
  */
-// eslint-disable-next-line import/no-unresolved
+// eslint-disable-next-line import/no-extraneous-dependencies
 import fetch from 'node-fetch';
+// eslint-disable-next-line import/no-unresolved
 import apiKey from './apikey.js';
 
 const tokenKey = `?apiKey=${apiKey}`;
