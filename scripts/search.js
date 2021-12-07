778b600ccb736d4fc82ed6a368983ddce01f07e7
eekaireb <56775442+eekaireb@users.noreply.github.com>
2021-12-06 21:21:28 -0800

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -6,7 +6,7 @@
  * Handles all queries related to spoonacular api.
  */
 // eslint-disable-next-line import/no-extraneous-dependencies
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 // eslint-disable-next-line import/no-unresolved
 import apiKey from './apikey.js';
 
