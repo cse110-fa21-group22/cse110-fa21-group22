fd5ffbbedeabcacf5d5ed5b945481162a0dc6cc9
HairforceNiko <fayuan@ucsd.edu>
2021-12-06 21:36:56 -0800

    Update end-to-end.test.js

--- source/__tests__/end-to-end.test.js
+++ source/__tests__/end-to-end.test.js
@@ -15,7 +15,7 @@ const FAV_SELECTED = 'https://icookfood.netlify.app/assets/favorite-selected.svg
 // Links for testing on PR
 const PREVIEW_TAG = 'https://deploy-preview-';
 const END_TAG = '--icookfood.netlify.app/';
-const prNum = 319; // change -> (your pr number);
+const prNum = 328; // change -> (your pr number);
 
 let homeLink = '';
 let searchLink = '';
