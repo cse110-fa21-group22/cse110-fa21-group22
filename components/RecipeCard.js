69a12c0de9d5d1560f91f3e66dcd0e008a0e5eb9
samanthaylin3 <73860504+samanthaylin3@users.noreply.github.com>
2021-12-06 16:11:25 -0800

    spelling cleanup

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -339,7 +339,7 @@ class RecipeCard extends HTMLElement {
           /**  **********************************  */
           // console.log(window.location.pathname);
           if (window.location.pathname === '/webpages/home.html') {
-            homeapgeRefreshFavoriteSection();
+            homepageRefreshFavoriteSection();
           }
           if (window.location.pathname === '/webpages/search.html') {
             searchpageRefreshSearchResult();
