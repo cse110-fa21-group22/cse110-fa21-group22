fce3041ea128bfd152a982a3f11eafb91b4a6654
samanthaylin3 <73860504+samanthaylin3@users.noreply.github.com>
2021-12-06 23:18:21 -0800

    change recipe card back to main

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -9,12 +9,11 @@ const recipeCardTemplate = document.createElement('template');
 recipeCardTemplate.innerHTML = `
   <article class="recipe-card">
     <img class="recipe-favorite" src="../assets/favorite.svg" alt="favorite" />
-    <img class="recipe-remove" src="../assets/favorite-remove.svg" alt="remove" />
     <img class="recipe-checkmark" src="../assets/checkmark.svg" alt="selected" />
     <img class="recipe-image"><img/>
     <div class="recipe-subdescription">
       <p class="recipe-name">Lorem ipsum dolor sit amet </p>
-      <p class="recipe-time"><span class="recipe-time-number">30</span> <span class="recipe-time-unit">minutes</span></p>
+      <p class="recipe-calories"><span class="recipe-calories-number">500</span> <span class="recipe-calories-unit">calories</span></p>
     </div>
   </article>
   <div class="dropdown-content">
@@ -36,21 +35,34 @@ listEntryTemplate.innerHTML = `
 `;
 
 class RecipeCard extends HTMLElement {
+  /**
+   * Setter for the recipe card's recipe
+   * @param {object} recipeObj The recipe object from spoonacular
+   */
   set recipe(recipeObj) {
     this.recipeObj = recipeObj;
     this.recipeId = recipeObj.id;
     const recipeImg = this.shadow.querySelector('.recipe-image');
-    const recipeTime = this.shadow.querySelector('.recipe-time-number');
+    const recipeCal = this.shadow.querySelector('.recipe-calories-number');
+    const recipeCalUnit = this.shadow.querySelector('.recipe-calories-unit');
     const recipeName = this.shadow.querySelector('.recipe-name');
-
     recipeImg.src = recipeObj.image;
     recipeName.innerHTML = recipeObj.title;
-    recipeTime.innerHTML = recipeTime.innerHTML.replace('30', recipeObj.readyInMinutes);
+    recipeCal.innerHTML = recipeCal.innerHTML.replace('500', recipeObj.nutrition.nutrients[0].amount);
+    recipeCalUnit.innerHTML = recipeCalUnit.innerHTML.replace('calories', recipeObj.nutrition.nutrients[0].unit);
 
     if (checkFavorite(recipeObj)) {
       this.isFavorite = true;
       this.initializeHearts();
     }
+  }
+
+  /**
+   * Getter for the recipe card's recipe
+   * @return {object} The recipe card's recipe object from Spoonacular
+   */
+  get recipe() {
+    return this.recipeObj;
   }
 
   /**
@@ -98,8 +110,8 @@ class RecipeCard extends HTMLElement {
    */
   enterSelectMode() {
     this.selectMode = true;
-    const favoriteRemove = this.shadow.querySelector('.recipe-remove');
-    favoriteRemove.style.display = 'block';
+    const favoriteIcon = this.shadow.querySelector('.recipe-favorite');
+    favoriteIcon.style.display = 'none';
   }
 
   /**
@@ -109,8 +121,11 @@ class RecipeCard extends HTMLElement {
   exitSelectMode() {
     this.selectMode = false;
     if (this.isSelected) this.deselect();
-    const favoriteRemove = this.shadow.querySelector('.recipe-remove');
-    favoriteRemove.style.display = 'none';
+    const checkmark = this.shadow.querySelector('.recipe-checkmark');
+    checkmark.style.display = 'none';
+    this.style.filter = 'brightness(100%)';
+    const favoriteIcon = this.shadow.querySelector('.recipe-favorite');
+    favoriteIcon.style.display = 'block';
   }
 
   /**
@@ -121,7 +136,7 @@ class RecipeCard extends HTMLElement {
     this.isSelected = true;
     checkmark.style.display = 'block';
     this.style.filter = 'brightness(90%)';
-    const event = new CustomEvent('selected', { detail: this.recipeId });
+    const event = new CustomEvent('selected', { detail: this });
     this.dispatchEvent(event);
   }
 
@@ -133,7 +148,7 @@ class RecipeCard extends HTMLElement {
     this.isSelected = false;
     checkmark.style.display = 'none';
     this.style.filter = 'brightness(100%)';
-    const event = new CustomEvent('deselected', { detail: this.recipeId });
+    const event = new CustomEvent('deselected', { detail: this });
     this.dispatchEvent(event);
   }
 
@@ -141,7 +156,7 @@ class RecipeCard extends HTMLElement {
    * Dispatches an event to remove this recipe
    */
   delete() {
-    const event = new CustomEvent('removed', { detail: this.recipeId });
+    const event = new CustomEvent('removed', { detail: this.recipeObj });
     this.dispatchEvent(event);
   }
 
@@ -213,7 +228,6 @@ class RecipeCard extends HTMLElement {
     // If the favorite icon is clicked, favorite the item
     const recipeCard = this;
     const favoriteIcon = this.shadow.querySelector('.recipe-favorite');
-    const favoriteRemove = this.shadow.querySelector('.recipe-remove');
     const submitFavorites = this.shadow.querySelector('.submit');
     const cancelFavorites = this.shadow.querySelector('.cancel');
     const dropdownContent = this.shadow.querySelector('.dropdown-content');
@@ -279,11 +293,10 @@ class RecipeCard extends HTMLElement {
       }
     });
 
-    /* Remove button event listener */
-    favoriteRemove.addEventListener('click', (event) => {
+    /* When click the favorite icon, dropdown content showup */
+    favoriteIcon.addEventListener('click', (event) => {
       event.stopPropagation();
-      console.log('Removing recipe from THIS list...');
-      this.delete();
+      this.showDropdown();
     });
 
     /* stops propagation of clicks on dropdown content box to the recipe card
