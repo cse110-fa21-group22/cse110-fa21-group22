f7c28d52142e997a6b2e2bce9a03b7aae11cda33
Emily Ekaireb <eekaireb@ucsd.edu>
2021-12-07 15:31:40 -0800

    changes tests

--- source/__tests__/unittest.test.js
+++ source/__tests__/unittest.test.js
@@ -1,3 +1,82 @@
 test('Comment in tests below to run unit tests locally', () => {
   expect(true).toBe(true);
 });
+
+/*
+import search from '../../scripts/search.js';
+test('search function test#1', () => {
+  const inputList = {};
+  inputList.query = 'eggs';
+  inputList.number = 10;
+  inputList.offset = 0;
+  inputList.sort = '';
+  inputList.recipeNutrition = 'true';
+  inputList.cuisineFilter = '';
+  inputList.dietFilter = '';
+  inputList.timeFilter = '';
+  inputList.typeFilter = '';
+  return search(inputList).then((value) => {
+    expect(value.totalResults).toBe(1596);
+  });
+});
+test('search function test#2', () => {
+  const inputList = {};
+  inputList.query = 'eggs';
+  inputList.number = 10;
+  inputList.offset = 0;
+  inputList.sort = '';
+  inputList.recipeNutrition = 'true';
+  inputList.cuisineFilter = 'Italian';
+  inputList.dietFilter = '';
+  inputList.timeFilter = '';
+  inputList.typeFilter = '';
+  return search(inputList).then((value) => {
+    expect(value.totalResults).toBe(108);
+  });
+});
+test('search function test#3', () => {
+  const inputList = {};
+  inputList.query = '';
+  inputList.number = 10;
+  inputList.offset = 0;
+  inputList.sort = '';
+  inputList.recipeNutrition = 'true';
+  inputList.cuisineFilter = '';
+  inputList.dietFilter = 'Vegan';
+  inputList.timeFilter = '';
+  inputList.typeFilter = '';
+  return search(inputList).then((value) => {
+    expect(value.totalResults).toBe(657);
+  });
+});
+test('search function test#4', () => {
+  const inputList = {};
+  inputList.query = '';
+  inputList.number = 10;
+  inputList.offset = 0;
+  inputList.sort = '';
+  inputList.recipeNutrition = 'true';
+  inputList.cuisineFilter = '';
+  inputList.dietFilter = '';
+  inputList.timeFilter = '';
+  inputList.typeFilter = '';
+  return search(inputList).then((value) => {
+    expect(value.totalResults).toBe(5076);
+  });
+});
+test('search function test#5', () => {
+  const inputList = {};
+  inputList.query = 'thiswillgivenoresults';
+  inputList.number = 10;
+  inputList.offset = 0;
+  inputList.sort = '';
+  inputList.recipeNutrition = 'true';
+  inputList.cuisineFilter = 'Italian';
+  inputList.dietFilter = '';
+  inputList.timeFilter = '';
+  inputList.typeFilter = '';
+  return search(inputList).then((value) => {
+    expect(value.totalResults).toBe(0);
+  });
+});
+*/
